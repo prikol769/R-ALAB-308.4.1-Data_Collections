18c63460e87fa0406f0b59b1f04a72c8446bc0d6
prikol769 <prikol769@gmail.com>
2024-06-06 11:18:50 -0500

    added Transforming Data

---
+++ transforming-data.js
@@ -0,0 +1,31 @@
+//Part 3: Transforming Data
+
+const arr = [
+  ["ID", "Name", "Occupation", "Age"],
+  ["42", "Bruce", "Knight", "41"],
+  ["57", "Bob", "Fry Cook", "19"],
+  ["63", "Blaine", "Quiz Master", "58"],
+  ["98", "Bill", "Doctor’s Assistant", "26"],
+];
+
+const CSV = (arr) => {
+  let numberOfColumns = arr[0].length;
+
+  const usersDataIntoObjects = [];
+
+  for (let i = 1; i < arr.length; i++) {
+    let currentObj = {};
+    for (let j = 0; j < arr[i].length; j++) {
+      currentObj = { ...currentObj, [arr[0][j].toLowerCase()]: arr[i][j] };
+    }
+    usersDataIntoObjects.push({
+      ...currentObj,
+    });
+    currentObj = {};
+  }
+
+  console.log("*********Transforming Data*********");
+  console.log("usersDataIntoObjects:", usersDataIntoObjects);
+};
+
+CSV(arr);
