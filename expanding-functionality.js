c60007c849ec3a0155cd44b20463b5e6d2603491
prikol769 <prikol769@gmail.com>
2024-06-06 11:18:23 -0500

    added Expanding Functionality

---
+++ expanding-functionality.js
@@ -0,0 +1,17 @@
+// Expanding Functionality
+
+const str =
+  "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+
+const CSV = (str) => {
+  let numberOfColumns = 0;
+
+  const usersData = str.split("\n").map((userData) => userData.split(","));
+  numberOfColumns = usersData[0].length;
+
+  console.log("*********Expanding Functionality*********");
+  console.log("usersData:", usersData);
+  console.log("numberOfColumns:", numberOfColumns);
+};
+
+CSV(str);
