aa528e80665d4b3ac419e527e33fb0185731750a
prikol769 <prikol769@gmail.com>
2024-06-06 11:23:32 -0500

    added input data log

--- expanding-functionality.js
+++ expanding-functionality.js
@@ -2,6 +2,8 @@
 
 const str =
   "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+console.log("input data:", str);
+console.log(" ");
 
 const CSV = (str) => {
   let numberOfColumns = 0;
